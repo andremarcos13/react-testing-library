05da27635c7fee4aad40ebf3f839b14079b56fb4
Andre Cesar <andrmgc@gmail.com>
2022-05-16 19:55:53 -0300

    adiciona test para notFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('Teste se a página contém um heading h2', () => {
+  renderWithRouter(<NotFound />);
+
+  const notFound = screen.getByRole(
+    'heading', { name: /Page requested not found Crying emoji/i, level: 2 },
+  );
+  const img = screen.getByRole('img',
+    { name: /Pikachu crying because the page requested was not found/i });
+
+  expect(notFound).toBeInTheDocument();
+  expect(img).toBeInTheDocument();
+  expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
