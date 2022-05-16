743edbb707ed1a14127b9931170a863c499ff715
Andre Cesar <andrmgc@gmail.com>
2022-05-16 17:02:52 -0300

    tentando resolver problema da importacao do renderWithROuter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouter from '../components/renderWithRouter';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 test('Teste se o topo da aplicação contém um conjunto fixo de links.', () => {
