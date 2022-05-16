4ca05ed0bca8389f52ad71eda4dfef7c80f6a663
Andre Cesar <andrmgc@gmail.com>
2022-05-16 16:53:31 -0300

    retirando describe

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,48 +4,46 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../components/renderWithRouter';
 import App from '../App';
 
-describe('Requisito - 1', () => {
-  test('Teste se o topo da aplicação contém um conjunto fixo de links.', () => {
-    renderWithRouter(<App />);
-    const linkHome = screen.getByRole('link', { name: /home/i });
-    const linkAbout = screen.getByRole('link', { name: /about/i });
-    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
-    expect(linkHome).toBeInTheDocument();
-    expect(linkAbout).toBeInTheDocument();
-    expect(linkFavorite).toBeInTheDocument();
-  });
+test('Teste se o topo da aplicação contém um conjunto fixo de links.', () => {
+  renderWithRouter(<App />);
+  const linkHome = screen.getByRole('link', { name: /home/i });
+  const linkAbout = screen.getByRole('link', { name: /about/i });
+  const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+  expect(linkHome).toBeInTheDocument();
+  expect(linkAbout).toBeInTheDocument();
+  expect(linkFavorite).toBeInTheDocument();
+});
 
-  test('Teste se a aplicação é redirecionada ao clicar no link home', () => {
-    const { history } = renderWithRouter(<App />);
-    const linkHome = screen.getByRole('link', { name: /home/i });
-    expect(linkHome).toBeInTheDocument();
-    userEvent.click(linkHome);
-    const { pathname } = history.location;
-    expect(pathname).toBe('/');
-  });
+test('Teste se a aplicação é redirecionada ao clicar no link home', () => {
+  const { history } = renderWithRouter(<App />);
+  const linkHome = screen.getByRole('link', { name: /home/i });
+  expect(linkHome).toBeInTheDocument();
+  userEvent.click(linkHome);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+});
 
-  test('Teste se a aplicação é redirecionada ao clicar no link about', () => {
-    const { history } = renderWithRouter(<App />);
-    const linkAbout = screen.getByRole('link', { name: /about/i });
-    expect(linkAbout).toBeInTheDocument();
-    userEvent.click(linkAbout);
-    const { pathname } = history.location;
-    expect(pathname).toBe('/about');
-  });
+test('Teste se a aplicação é redirecionada ao clicar no link about', () => {
+  const { history } = renderWithRouter(<App />);
+  const linkAbout = screen.getByRole('link', { name: /about/i });
+  expect(linkAbout).toBeInTheDocument();
+  userEvent.click(linkAbout);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+});
 
-  test('Teste se a aplicação é redirecionada ao clicar no link favorite', () => {
-    const { history } = renderWithRouter(<App />);
-    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
-    expect(linkFavorite).toBeInTheDocument();
-    userEvent.click(linkFavorite);
-    const { pathname } = history.location;
-    expect(pathname).toBe('/favorites');
-  });
+test('Teste se a aplicação é redirecionada ao clicar no link favorite', () => {
+  const { history } = renderWithRouter(<App />);
+  const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+  expect(linkFavorite).toBeInTheDocument();
+  userEvent.click(linkFavorite);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+});
 
-  test('Teste se a aplicação é redirecionada para a página Not Found', () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/nao encontrei nada!');
-    const notFound = screen.getByRole('heading', { name: 'Nothing here' });
-    expect(notFound).toBeInTheDocument();
-  });
+test('Teste se a aplicação é redirecionada para a página Not Found', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/nao encontrei nada!');
+  const notFound = screen.getByRole('heading', { name: 'Nothing here' });
+  expect(notFound).toBeInTheDocument();
 });
